a3a26b89f5c8836274056b1bf3d1722a6409d7c0
Julio César Méndez Marante <mendezmarantejuliocesar@gmail.com>
2024-03-10 06:41:56 +0100

    initial commit

---
+++ access/js/abrircerrar.js
@@ -0,0 +1,66 @@
+
+const btnToggle = document.querySelector('.btn_toggle');
+
+btnToggle.addEventListener('click', function () {
+  document.getElementById('contenido').classList.toggle('active');
+})
+
+
+
+const btnList_1 = document.querySelector('.firstask');
+const btnList_2 = document.querySelector('.secoundask');
+const btnList_3 = document.querySelector('.threehask');
+const btnList_4 = document.querySelector('.fourask');
+const btnList_5 = document.querySelector('.fiveask');
+const btnList_6 = document.querySelector('.sixask');
+const btnList_7 = document.querySelector('.sevenask');
+const btnList_8 = document.querySelector('.eighask');
+const btnList_9 = document.querySelector('.nineask');
+const btnList_0 = document.querySelector('.tenask');
+
+btnList_1.addEventListener('click', function () {
+  document.getElementById('costo').classList.toggle('active');
+});
+
+btnList_2.addEventListener('click', function () {
+  document.getElementById('diferencia').classList.toggle('active');
+});
+
+btnList_3.addEventListener('click', function () {
+  document.getElementById('hosting').classList.toggle('active');
+});
+
+btnList_4.addEventListener('click', function () {
+  document.getElementById('dominio').classList.toggle('active');
+});
+
+btnList_5.addEventListener('click', function () {
+  document.getElementById('page').classList.toggle('active');
+});
+
+btnList_6.addEventListener('click', function () {
+  document.getElementById('later').classList.toggle('active');
+});
+
+btnList_7.addEventListener('click', function () {
+  document.getElementById('changes').classList.toggle('active');
+});
+
+btnList_8.addEventListener('click', function () {
+  document.getElementById('country').classList.toggle('active');
+});
+
+btnList_9.addEventListener('click', function () {
+  document.getElementById('shop').classList.toggle('active');
+});
+
+btnList_0.addEventListener('click', function () {
+  document.getElementById('optimation').classList.toggle('active');
+});
+
+
+
+
+
+
+
